439b3ecbcf8af074624ebe9f81f6db0ef1654f31
fc_botelho <fc_botelho>
2005-01-24 19:11:08 +0000

    The algorithms and hash functions were put in alphabetical order

--- src/cmph_types.h
+++ src/cmph_types.h
@@ -5,10 +5,10 @@ typedef unsigned char cmph_uint8;
 typedef unsigned short cmph_uint16;
 typedef unsigned int cmph_uint32;
 
-typedef enum { CMPH_HASH_JENKINS, CMPH_HASH_DJB2, CMPH_HASH_SDBM, CMPH_HASH_FNV, 
-	       CMPH_HASH_GLIB, CMPH_HASH_PJW, CMPH_HASH_COUNT } CMPH_HASH;
+typedef enum { CMPH_HASH_DJB2, CMPH_HASH_FNV, CMPH_HASH_GLIB, CMPH_HASH_JENKINS, 
+	       CMPH_HASH_PJW, CMPH_HASH_SDBM, CMPH_HASH_COUNT } CMPH_HASH;
 extern const char *cmph_hash_names[];
-typedef enum { CMPH_CZECH, CMPH_BMZ, CMPH_COUNT } CMPH_ALGO; /* included -- Fabiano */
+typedef enum { CMPH_BMZ, CMPH_CZECH, CMPH_COUNT } CMPH_ALGO; /* included -- Fabiano */
 extern const char *cmph_names[];
 
 #endif
