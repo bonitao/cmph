c98912e0aec64f91108b89b917c68cbc761a5dc1
fc_botelho <fc_botelho>
2005-07-29 03:09:31 +0000

    it was fixed more mistakes in BRZ algorithm

--- src/djb2_hash.h
+++ src/djb2_hash.h
@@ -11,6 +11,7 @@ typedef struct __djb2_state_t
 djb2_state_t *djb2_state_new();
 cmph_uint32 djb2_hash(djb2_state_t *state, const char *k, cmph_uint32 keylen);
 void djb2_state_dump(djb2_state_t *state, char **buf, cmph_uint32 *buflen);
+djb2_state_t *djb2_state_copy(djb2_state_t *src_state);
 djb2_state_t *djb2_state_load(const char *buf, cmph_uint32 buflen);
 void djb2_state_destroy(djb2_state_t *state);
 
--- src/hash.h
+++ src/hash.h
@@ -8,6 +8,7 @@ typedef union __hash_state_t hash_state_t;
 hash_state_t *hash_state_new(CMPH_HASH, cmph_uint32 hashsize);
 cmph_uint32 hash(hash_state_t *state, const char *key, cmph_uint32 keylen);
 void hash_state_dump(hash_state_t *state, char **buf, cmph_uint32 *buflen);
+hash_state_t * hash_state_copy(hash_state_t *src_state);
 hash_state_t *hash_state_load(const char *buf, cmph_uint32 buflen);
 void hash_state_destroy(hash_state_t *state);
 
