328e2c10b285da3ba79a2f019c740f2ed9b5548c
fc_botelho <fc_botelho>
2008-03-23 00:46:34 +0000

    *** empty log message ***

--- src/hash_state.h
+++ src/hash_state.h
@@ -3,16 +3,10 @@
 
 #include "hash.h"
 #include "jenkins_hash.h"
-#include "djb2_hash.h"
-#include "sdbm_hash.h"
-#include "fnv_hash.h"
 union __hash_state_t
 {
 	CMPH_HASH hashfunc;
 	jenkins_state_t jenkins;
-	djb2_state_t djb2;
-	sdbm_state_t sdbm;
-	fnv_state_t fnv;
 };
 
 #endif
